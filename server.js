9e72e0cb07d5599e799f7976cc108be94d50e777
Omer <guesttt@Zainabs-Air.mynetworksettings.com>
2024-12-26 14:15:03 -0600

    uninstalled helmet properly

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const helmet = require('helmet');
+//const helmet = require('helmet');
 
 // Initialize Express
 const app = express();
