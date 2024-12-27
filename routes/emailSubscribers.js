6caa0e2ff6b688d5998d8c5efe24c2c9c3e83194
aamish-DMS <130136756+aamish-DMS@users.noreply.github.com>
2024-12-27 12:56:22 +0300

    test

--- routes/emailSubscribers.js
+++ routes/emailSubscribers.js
@@ -7,7 +7,6 @@ const router = express.Router();
 router.post('/subscribe', async (req, res) => {
     try {
         const { email } = req.body;
-
         if (!email) {
             return res.status(400).json({ error: 'Email is required.' });
         }
