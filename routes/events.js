4e05639fe59a4415e746446573cc36a3ab5e4a2d
fatima-codes0 <kirvehari98@gmail.com>
2026-02-13 00:26:54 +0300

    changes

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose'); // Import mongoose
 
+
 router.get("/", async (req, res) => {
     try {
         const now = new Date();
